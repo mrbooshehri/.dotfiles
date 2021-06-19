b9cba39ac2992485718d8104b7690055c26bc9a2
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-06-19 18:06:17 +0300

    minor edits

--- emacs/.config/emacs/elisp/theme.el
+++
@@ -1,5 +0,0 @@
-(use-package spacemacs-theme
-  :defer t
-  :init
-  (load-theme 'spacemacs-dark t))
-(provide 'theme)
--- emacs/.config/emacs/elpa/org-plus-contrib-20210412/org-plus-contrib-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "org-plus-contrib"
-  "20210412" "Outline-based notes management and organizer" ( ))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
